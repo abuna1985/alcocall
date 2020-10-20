a3275c2540fbe042268558d0bea5414e1b35e594
Amalia Hay <amaliawhay@gmail.com>
2020-10-19 21:12:33 -0700

    bodyParser set to true

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Passport middleware
